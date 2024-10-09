a123a483bbc6116887139eff43696e282ab51256
laksmanayudha <laksmanayudha22@gmail.com>
2024-10-09 23:38:03 +0800

    setup route handler

---
+++ src/handler.js
@@ -0,0 +1,27 @@
+const addBook = (request, h) => {
+  return h.response();
+}
+
+const getAllBook = () => {
+
+}
+
+const getBookDetail = () => {
+
+}
+
+const updateBook = () => {
+
+}
+
+const deleteBook = () => {
+
+}
+
+module.exports = {
+  addBook,
+  getAllBook,
+  getBookDetail,
+  updateBook,
+  deleteBook
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,31 @@
-const routes = [
+const handler = require('./handler');
 
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: handler.storeBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: handler.getAllBook,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: handler.getBookDetail,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: handler.updateBook,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: handler.deleteBook,
+  },
 ];
 
 module.exports = routes;
